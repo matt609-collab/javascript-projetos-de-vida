d386218f621f6baec4c1de127cef027701c1003b
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-07-29 10:58:03 +0000

    return

--- main.js
+++ main.js
@@ -14,6 +14,20 @@ for(let i = 0; i< botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05t00:00:00");
-let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 59;
+    minutos %= 59;
+    horas %= 23;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
