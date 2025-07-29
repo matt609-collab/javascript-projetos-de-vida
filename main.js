81771fd2db7776823060e153ac4d046dff55ec40
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-07-29 11:17:09 +0000

    complete rewrite of everything cause my code sucks

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-const botoes = document.querySelectorAll(".button");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i = 0; i< botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j = 0; j < botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
+
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05t00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
-function calculaTempo(tempoObjetivo){
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
@@ -25,9 +28,9 @@ function calculaTempo(tempoObjetivo){
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
-    segundos %= 59;
-    minutos %= 59;
-    horas %= 23;
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
 
-    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
