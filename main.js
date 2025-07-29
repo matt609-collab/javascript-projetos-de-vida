9d9e5b8eea38159b8a113c848fc9ad1a3e5aaef2
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-07-29 10:42:40 +0000

    i dont have a good name for this commit

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 const botoes = document.querySelectorAll(".button");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i = 0; i < botoes.length; i++){
+for(let i = 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
-
-        for(let j = 0; i < botoes.length; j++){
+        for(let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
-
         }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
-
 }
 
 const contadores = document.querySelectorAll(".contador");
