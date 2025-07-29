47fc1525471afe8d2e8f50a8a6b9cfab630a2de0
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-07-29 10:32:15 +0000

    dom

--- main.js
+++ main.js
@@ -14,3 +14,9 @@ for(let i = 0; i < botoes.length; i++){
     }
 
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-10-05t00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
